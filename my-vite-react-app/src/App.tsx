5289f008a258683d69455994d4fa8da052aebfc5
Saad AKLLAM <63886515+EVBLOOD@users.noreply.github.com>
2023-10-13 18:28:30 +0100

    update the template and interface

--- my-vite-react-app/src/App.tsx
+++ my-vite-react-app/src/App.tsx
@@ -5,32 +5,38 @@ import axios from 'axios';
 function App() {
 
   // aws gateway link.
-  const link = '';
-  let link_;
+  const link = 'https://1nnjgw5pva.execute-api.us-east-1.amazonaws.com/dev/api';
 
-  const [buckets, setBuckets] = useState([]);
-  const [objects, setObjects] = useState([]);
-  const [object, setObject] = useState(null);
-  const [backet, setBacket] = useState("");
-  const [key, setKey] = useState("");
+  let link_; // link to fetch with
+
+  const [buckets, setBuckets] = useState([]); // buckets in aws console
+  const [objects, setObjects] = useState([]); // objects inside the selected bucket
+  const [object, setObject] = useState(""); // the object fetched from the bucket&key
+  const [backet, setBacket] = useState(""); // selected bucket
+  const [key, setKey] = useState(""); // selected object key
 
   useEffect(() => {
-    link_ = (backet.length && key.length) ? `${link}?backet=${backet}&key=${key}` : backet.length ? `${link}?backet=${backet}` : `${link}`;
+    setObject("")
+    link_ = (backet.length && key.length) ? `${link}?bucket=${backet}&key=${key}` : backet.length ? `${link}?bucket=${backet.toString()}` : `${link}`;
+    console.log(link_);
     axios.get(link_).then((resp: any) => {
       console.log(resp);
-      (backet.length && key.length) ? setObject(resp) : backet.length ? setObjects(resp) : setBuckets(resp);
+      console.log("loop");
+      (backet.length && key.length) ? setObject(resp.data) : backet.length ? setObjects(resp.data.substring(1, resp.data.length - 1).split(',')) : setBuckets(resp.data.substring(1, resp.data.length - 1).split(','));
+      setBacket("")
+      setKey("")
     }).catch((resp) => { console.log(resp); });
-  }, [])
+  }, [backet, key])
   return (
     <>
       <h1 style={{ color: 'grey' }}>Workshop 6</h1>
       <div>
         <h1 style={{ color: 'red' }}>buckets</h1>
         {
-          buckets.length ?
-            <select name="buckets">
+          buckets.length > 0 ?
+            <select name="buckets" onChange={(event) => setBacket(event.target.value)}>
               {
-                buckets.map((buc) => { return <option value={buc}>{buc}</option> })
+                buckets.map((buc: string) => { return <option key={buc.trim().substring(1, buc.trim().length - 1)} value={buc.trim().substring(1, buc.trim().length - 1)}>{buc.trim().substring(1, buc.trim().length - 1)}</option> })
               }
             </select>
             : <h1>no buckets or you have an error in the aws gateway</h1>
@@ -39,20 +45,17 @@ function App() {
       <div>
         <h1 style={{ color: 'red' }}>objectKeys</h1>
         {
-          objects.length ?
-            <select name="objectKeys">
-              {objects.map((obj) => { return <option value={obj}>{obj}</option> })}
+          objects.length > 0 ?
+            <select name="objectKeys" onChange={(event) => setKey(event.target.value)}>
+              <option value=""></option>
+              {objects.map((obj: string) => { return <option key={obj.trim().substring(1, obj.trim().length - 1)} value={obj.trim().substring(1, obj.trim().length - 1)}>{obj.trim().substring(1, obj.trim().length - 1)}</option> })}
             </select>
             : <h1>no objects or none selected</h1>
         }
       </div>
       <div>
         {
-          object &&
-          <div>
-            <h1>Selected Object</h1>
-            {object}
-          </div>
+          object && JSON.stringify(object)
         }
       </div>
     </>
--- script.py
+++ script.py
@@ -1,35 +1,75 @@
 import boto3
 import json
-
-s3 = boto3.client('s3')
-
 def lambda_handler(event, context):
+  s3 = boto3.client('s3')
+  if 'queryStringParameters' in event and event['queryStringParameters'] is not None and 'bucket' in event['queryStringParameters'] :
     bucket_name = event['queryStringParameters']['bucket']
-    key_object = event['queryStringParameters']['key']
-    
-    if bucket_name:
-        try:
-            response = s3.get_object(Bucket=bucket_name, Key=key_object)
-            data = response['Body'].read().decode('utf-8')
-            return {
-                'statusCode': 200,
-                'body': json.dumps(data)
+    if 'key' in event['queryStringParameters'] :
+      object_key = event['queryStringParameters']['key']
+      try:
+        response = s3.get_object(Bucket=bucket_name, Key=object_key)
+        return {
+          'statusCode': 200,
+          'headers': {
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Origin': 'http://localhost:5173',
+            'Access-Control-Allow-Methods': 'GET'
+          },
+          'body':  json.loads(json.dumps(str(response['Body'].read())))
+        }
+      except Exception as e:
+        return {
+            'statusCode': 404,
+            'headers': {
+              'Access-Control-Allow-Headers': 'Content-Type',
+              'Access-Control-Allow-Origin': 'http://localhost:5173',
+              'Access-Control-Allow-Methods': 'GET'
+            },
+            'body': json.dumps(str(e))
+        }
+    else :
+      try:
+        response = [object_key['Key'] for object_key in s3.list_objects_v2(Bucket=bucket_name)['Contents']]
+        return {
+            'statusCode': 200,
+            'headers': {
+              'Access-Control-Allow-Headers': 'Content-Type',
+              'Access-Control-Allow-Origin': 'http://localhost:5173',
+              'Access-Control-Allow-Methods': 'GET'
+            },
+            'body': json.loads(json.dumps(str(response)))
             }
-        except Exception as e:
-            return {
-                'statusCode': 404,
-                'body': json.dumps(str(e))
-            }
-    else:
-        try:
-            response = s3.list_buckets()
-            data = response['Buckets']
-            return {
-                'statusCode': 200,
-                'body': json.dumps(data)
-            }
-        except Exception as e:
-            return {
-                'statusCode': 404,
-                'body': json.dumps(str(e))
-            }
+      except Exception as e:
+        return {
+          'statusCode': 404,
+          'headers': {
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Origin': 'http://localhost:5173',
+            'Access-Control-Allow-Methods': 'GET'
+          },
+          'body': json.loads(json.dumps(str(e)))
+        }
+  else :
+    try:
+      response = s3.list_buckets()
+      print(response)
+      data = [bucket['Name'] for bucket in response['Buckets']]
+      return {
+        'statusCode': 200,
+        'headers': {
+          'Access-Control-Allow-Headers': 'Content-Type',
+          'Access-Control-Allow-Origin': 'http://localhost:5173',
+          'Access-Control-Allow-Methods': 'GET'
+        },
+        'body': json.loads(json.dumps(str(data)))
+      }
+    except Exception as e:
+      return {
+          'statusCode': 404,
+          'headers': {
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Origin': 'http://localhost:5173',
+            'Access-Control-Allow-Methods': 'GET'
+          },
+          'body': json.dumps(str(e))
+      }
