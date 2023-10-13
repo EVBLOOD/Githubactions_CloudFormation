0b8a6e9c2f8d9a1bdbe111bdecb8b9842d42d479
Saad AKLLAM <63886515+EVBLOOD@users.noreply.github.com>
2023-10-13 18:56:13 +0100

    refuctoring the code

--- my-vite-react-app/src/App.tsx
+++ my-vite-react-app/src/App.tsx
@@ -9,24 +9,40 @@ function App() {
 
   let link_; // link to fetch with
 
-  const [buckets, setBuckets] = useState([]); // buckets in aws console
-  const [objects, setObjects] = useState([]); // objects inside the selected bucket
+  const [buckets, setBuckets] = useState<string[]>([]); // buckets in aws console
+  const [objects, setObjects] = useState<string[]>([]); // objects inside the selected bucket
   const [object, setObject] = useState(""); // the object fetched from the bucket&key
   const [backet, setBacket] = useState(""); // selected bucket
   const [key, setKey] = useState(""); // selected object key
 
+  function helperToArray(target: String): string[] {
+    return target.substring(1, target.length - 1).split(',')
+  }
+  function helperToValidName(target: String): string {
+    return target.trim().substring(1, target.trim().length - 1)
+  }
+
   useEffect(() => {
     setObject("")
-    link_ = (backet.length && key.length) ? `${link}?bucket=${backet}&key=${key}` : backet.length ? `${link}?bucket=${backet.toString()}` : `${link}`;
-    console.log(link_);
+    link_ = (backet.length && key.length) ? `${link}?bucket=${backet}&key=${key}` : // if I have both the bucketname and Objectkey I'll get that object..
+      backet.length ? `${link}?bucket=${backet.toString()}` : // if I have only the bucketname I'll get only the Objects names of that bucket
+        `${link}`; // if I don't have none, I'll just get a list if my aws buckets
+    // here I fetch data from the apiGateway
     axios.get(link_).then((resp: any) => {
       console.log(resp);
       console.log("loop");
-      (backet.length && key.length) ? setObject(resp.data) : backet.length ? setObjects(resp.data.substring(1, resp.data.length - 1).split(',')) : setBuckets(resp.data.substring(1, resp.data.length - 1).split(','));
+      (backet.length && key.length) ? setObject(resp.data) : // an object body 
+        backet.length ? setObjects(helperToArray(resp.data)) // a bucket object names
+          : setBuckets(helperToArray(resp.data)); // bucket names
       setBacket("")
       setKey("")
-    }).catch((resp) => { console.log(resp); });
+    }).catch((resp) => {
+      console.error(resp);
+    });
   }, [backet, key])
+
+
+
   return (
     <>
       <h1 style={{ color: 'grey' }}>Workshop 6</h1>
@@ -34,9 +50,14 @@ function App() {
         <h1 style={{ color: 'red' }}>buckets</h1>
         {
           buckets.length > 0 ?
-            <select name="buckets" onChange={(event) => setBacket(event.target.value)}>
+            <select name="buckets"
+              onChange={(event) => setBacket(event.target.value)}>
+              <option value=""></option>
               {
-                buckets.map((buc: string) => { return <option key={buc.trim().substring(1, buc.trim().length - 1)} value={buc.trim().substring(1, buc.trim().length - 1)}>{buc.trim().substring(1, buc.trim().length - 1)}</option> })
+                buckets.map((buc: string) => {
+                  return <option key={helperToValidName(buc)} value={helperToValidName(buc)}>{helperToValidName(buc)}
+                  </option>
+                })
               }
             </select>
             : <h1>no buckets or you have an error in the aws gateway</h1>
@@ -46,9 +67,14 @@ function App() {
         <h1 style={{ color: 'red' }}>objectKeys</h1>
         {
           objects.length > 0 ?
-            <select name="objectKeys" onChange={(event) => setKey(event.target.value)}>
+            <select name="objectKeys"
+              onChange={(event) => setKey(event.target.value)}>
               <option value=""></option>
-              {objects.map((obj: string) => { return <option key={obj.trim().substring(1, obj.trim().length - 1)} value={obj.trim().substring(1, obj.trim().length - 1)}>{obj.trim().substring(1, obj.trim().length - 1)}</option> })}
+              {
+                objects.map((obj: string) => {
+                  return <option key={helperToValidName(obj)} value={helperToValidName(obj)}>{helperToValidName(obj)}</option>
+                })
+              }
             </select>
             : <h1>no objects or none selected</h1>
         }
